6086d57874ded404158308676cc70c5774fcb365
billchetch <45006134+billchetch@users.noreply.github.com>
2025-01-14 21:29:23 +0700

    sss

---
+++ WorkerServiceChetchXMPP/chetch-worker-xmpp-test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Run the service as a FDD
+/home/pi/.dotnet/dotnet /home/pi/Documents/VisualStudioProjects/Tests/WorkerServiceChetchXMPP/WorkerServiceChetchXMPP/bin/Debug/net9.0/WorkerServiceChetchXMPP.dll
+#/home/pi/.dotnet/dotnet --version
+#echo "Hi there"
